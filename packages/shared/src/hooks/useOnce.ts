7c6a6ecbce2def03833a84a57c96f84b9c04e692
Josh <37798644+joshuaellis@users.noreply.github.com>
2021-06-09 22:18:46 +0100

    fix: useOnce type annotation

--- packages/shared/src/hooks/useOnce.ts
+++ packages/shared/src/hooks/useOnce.ts
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 
 // Explicit type annotation fixes TS2742 error.
-type UseOnce = (effect: React.EffectCallback) => void
+type UseOnce = (effect: () => any | (() => any)) => void
 
 export const useOnce: UseOnce = effect => useEffect(effect, emptyDeps)
 
